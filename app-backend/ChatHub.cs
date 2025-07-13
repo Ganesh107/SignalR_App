3dde813e08c7d549be38883a4868bd39d11ca75c
Ganesh107 <sganesh371@gmail.com>
2025-07-13 21:58:44 +0530

    signalR backend changes

---
+++ app-backend/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace app_backend
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task UserTyping(string user)
+        {
+            await Clients.Others.SendAsync("UserTyping", user);
+        }
+    }
+}
